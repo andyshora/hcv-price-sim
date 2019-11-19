61801d7fb7b8b95b92eeb81684a6f064e4539acf
Andy Shora <shora.andy@bcg.com>
2019-11-19 11:56:34 +0000

    Cherrypicked changes from v2 branch.

--- src/components/cost-breakdown/CostBreakdown.js
+++ src/components/cost-breakdown/CostBreakdown.js
@@ -33,7 +33,7 @@ const TitleWrap = styled.div`
 const ValueLabel = styled.text`
   // font-size: ${props =>
     props.val < 0.06 ? Math.max(1, 1.6 * (props.val / 0.1)) : 1.6}rem;
-  font-size: 1.4rem;
+  font-size: 1.25rem;
   fill: white;
   width: ${props => props.width}px;
   user-select: none;
